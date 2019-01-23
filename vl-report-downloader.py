9a997ed4f94b1b7696a8166de42e7ffa7b5a9db2
Patrick Sharkey <patrick.sharkey@nextcentury.com>
2019-01-22 20:13:02 -0500

    implemented move files method

--- vl-report-downloader.py
+++ vl-report-downloader.py
@@ -3,6 +3,7 @@ import glob
 import time
 import os
 import multiprocessing
+import shutil
 
 from multiprocessing import current_process
 from selenium import webdriver
@@ -13,11 +14,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class Worker(multiprocessing.Process):
-    def __init__(self, base_download_path: str, timeout: int, queue: multiprocessing.JoinableQueue):
+    def __init__(self, base_download_path: str, dest_path: str, timeout: int, queue: multiprocessing.JoinableQueue):
         """
         """
         multiprocessing.Process.__init__(self)
         self.base_download_path = base_download_path
+        self.dest_path = dest_path
         self.timeout = timeout
         self.queue = queue
         self.stop_event = multiprocessing.Event()
@@ -35,13 +37,12 @@ class Worker(multiprocessing.Process):
         # attempt to log in
         self.is_loggedin = self.__login()
 
-        #TODO implement logging in one time only
         #TODO implement moving files to new folder with ticker name
         while not self.stop_event.is_set() and self.is_loggedin:
 
             if not self.queue.empty():
                 ticker = self.queue.get()
-        
+
                 # execute search and download
                 self.__search(self.driver, ticker)
             else:
@@ -53,7 +54,7 @@ class Worker(multiprocessing.Process):
 
     def __login(self):
 
-        print("[{0}] is logging in to the system".format(current_process().name))
+        print("[{0}] logging in to the system".format(current_process().name))
 
         #set up chrome optins to download files
         options = webdriver.ChromeOptions()
@@ -98,11 +99,8 @@ class Worker(multiprocessing.Process):
         """
         """
         try:
+            driver.get(config.RESET_URL)
 
-            #TODO make this config?
-            driver.get('https://research-valueline-com.library.access.arlingtonva.us/secure/research')
-
-            # wait maximum 20 seconds for SSO login/redirect to occur
             #TODO catch timeout exception
             WebDriverWait(driver, self.timeout).until(
                 EC.title_is("Value Line - Research - Browse Research")
@@ -116,7 +114,7 @@ class Worker(multiprocessing.Process):
     def __search(self, driver: webdriver, ticker: str):
         """
         """
-        print("[{0}] is searching for {1}".format(current_process().name, ticker))
+        print("[{0}] searching for {1}".format(current_process().name, ticker))
         
         # navigate to the first stock page
         #TODO injecting value causes issue
@@ -137,6 +135,9 @@ class Worker(multiprocessing.Process):
             link = anchor.get_attribute("href")
             self.__download(driver, ticker, link, anchor.text)
 
+        # move the files
+        self.__move_files(ticker)
+
         # reset the search
         self.__reset(driver, ticker)
 
@@ -150,7 +151,7 @@ class Worker(multiprocessing.Process):
         # execute download
         driver.get(link)
     
-        print("[{0}] is downloading {1}-{2}.pdf".format(current_process().name, ticker, anchor_text))
+        print("[{0}] downloading {1}-{2}.pdf".format(current_process().name, ticker, anchor_text))
         dl_count = len(glob.glob1(self.base_download_path, partial_file))
 
         # max dl wait time ~5 seconds
@@ -163,31 +164,47 @@ class Worker(multiprocessing.Process):
                 dl_wait -= 1 #decrement wait 
                 dl_count = len(glob.glob1(self.base_download_path, partial_file))
                 self.__rename_file(filename)
-            else:
-                #TODO pass wait seconds as parameter
-                
+            else:                
                 print("[{0}] error downloading {1}, file did not finishing downloading witin 5 seconds".format(current_process().name, filename))
                 #TODO add error to the error queue
                 
-
     def __rename_file(self, filename: str):
         """
         """
         default_name = "report.pdf" #default download name
 
         if glob.glob(self.base_download_path + default_name):
-            os.rename(self.base_download_path + default_name, self.base_download_path  + filename)
+            os.rename(self.base_download_path + default_name, self.base_download_path  + filename + ".pdf") 
         else:
             print("[{0}] error renaming file, file {1} does not exist".format(current_process().name, self.base_download_path + default_name))
             #TODO add error to error queue
 
+    def __move_files(self, ticker: str):
+        """
+        """
+        # find all downloaded ticker files and move thme
+        files = glob.glob('{0}{1}*.pdf'.format(self.base_download_path, ticker))
+
+        print("Files found: " + str(files))
+
+        if files:
+            dest_ticker_path = '{0}{1}'.format(self.dest_path, ticker)
+
+            # create directory if it does not exist
+            if not os.path.exists(dest_ticker_path):
+                os.makedirs(dest_ticker_path)
+
+            # move the files
+            for f in files:
+                shutil.move(f, dest_ticker_path)
+                print("[{0}] moved {1} to {2}".format(current_process().name, f, dest_ticker_path))
 def main():
     """
     """
     work_queue = multiprocessing.JoinableQueue()
     work_queue.put('AAPL')
     work_queue.put('MSFT')
-    worker = Worker(config.BASE_DOWNLOAD_PATH, config.TIMEOUT, work_queue)
+    worker = Worker(config.BASE_DOWNLOAD_PATH, config.DEST_PATH, config.TIMEOUT, work_queue)
     worker.start()
 
 
